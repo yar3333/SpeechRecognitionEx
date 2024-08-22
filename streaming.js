a72a9d68e954425c365ccef96f6434a51bade3d6
Lethargicloli <harper.connor@gmail.com>
2024-08-22 15:40:39 +0100

    Converted tabs to spaces, parsed the input Int and set default to 0

--- streaming.js
+++ streaming.js
@@ -11,7 +11,7 @@ class StreamingSttProvider {
     settings;
 
     defaultSettings = {
-		triggerCharMin: 1,
+		triggerCharMin: 0,
         triggerWordsText: '',
         triggerWords: [],
         triggerWordsEnabled: false,
@@ -23,8 +23,8 @@ class StreamingSttProvider {
     get settingsHtml() {
         let html = '\
         <div id="speech_recognition_streaming_trigger_words_div">\
-		<span>Minimum spoken characters to trigger</span><br>\
-		<input max="99" min="0" class="text_pole" id="speech_recognition_streaming_trigger_min_chars" type="number"><br>\
+        <span>Minimum spoken characters to trigger</span><br>\
+        <input max="99" min="0" class="text_pole" id="speech_recognition_streaming_trigger_min_chars" type="number"><br>\
         <span>Trigger words</span>\
         <textarea id="speech_recognition_streaming_trigger_words" class="text_pole textarea_compact" type="text" rows="4" placeholder="Enter comma separated words that triggers new message, example:\nhey, hey aqua, record, listen"></textarea>\
         <label class="checkbox_label" for="speech_recognition_streaming_trigger_words_enabled">\
@@ -45,7 +45,7 @@ class StreamingSttProvider {
     }
 
     onSettingsChange() {
-		this.settings.triggerCharMin = $('#speech_recognition_streaming_trigger_min_chars').val();
+		this.settings.triggerCharMin = Number.parseInt($('#speech_recognition_streaming_trigger_min_chars').val());
         this.settings.triggerWordsText = $('#speech_recognition_streaming_trigger_words').val();
         let array = $('#speech_recognition_streaming_trigger_words').val().split(',');
         array = array.map(element => { return element.trim().toLowerCase(); });
@@ -115,7 +115,7 @@ class StreamingSttProvider {
         const data = await apiResult.json();
 		//Return the transcript if it exceeds the minimum character number.
 		if (data.transcript.length > this.settings.triggerCharMin) {
-        return data.transcript;
+        return data.transcript + this.settings.triggerCharMin + data.transcript.length;
 		}
     }
 
